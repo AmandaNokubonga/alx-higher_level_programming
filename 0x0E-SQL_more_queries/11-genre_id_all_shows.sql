81e0b9f242e7ef6a0577dea591f3db59560f0e9c
AmandaNokubonga <amandamagwaza3@gmail.com>
2023-12-14 12:16:40 -0800

    genre

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,10 @@
-SELECT tv_shows.title, COALESCE(GROUP_CONCAT(tv_show_genres.genre_id SEPARATOR ', '), 'No genres listed') AS genre_ids
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-GROUP BY tv_shows.id, tv_shows.title
-ORDER BY tv_shows.title;
+-- lists all shows contained in the database hbtn_0d_tvshows
+   -- Each record should display: tv_shows.title - tv_show_genres.genre_id
+   -- Results must be sorted in ascending order by tv_shows.title
+   -- and tv_show_genres.genre_id
+   -- If a show doesn’t have a genre, display NULL
+   -- The database name will be passed as an argument of the mysql command
+
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
