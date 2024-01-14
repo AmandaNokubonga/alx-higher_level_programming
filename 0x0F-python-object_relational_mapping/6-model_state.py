203898d54c9a4ba609cd847461704cfabe96617d
AmandaNokubonga <amandamagwaza3@gmail.com>
2024-01-14 12:23:00 -0800

    gogo

--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -4,10 +4,10 @@ This script defines a State class and
 a Base class to work with MySQLAlchemy ORM.
 """
 import sys
-from model_state import Base, State
+from model__state import Base, State
 
 from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    engine = create__engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool__pre__ping=True)
+    Base.metadata.create__all(engine)
