91bb03474bac9c96d77cfedf48e0f6d08f60e8e5
AmandaNokubonga <amandamagwaza3@gmail.com>
2024-01-14 12:46:19 -0800

    xohope

--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -6,8 +6,15 @@ a Base class to work with MySQLAlchemy ORM.
 import sys
 from model__state import Base, State
 
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 
 if __name__ == "__main__":
-    engine = create__engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool__pre__ping=True)
-    Base.metadata.create__all(engine)
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+    Base.metadata.create_all(engine)
